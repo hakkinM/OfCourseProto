8ad93cf1ee899ab3ddd206d359ff1b12c03ff56e
Urho <urho.lohikoski@gmail.com>
2025-02-26 12:12:38 +0200

    Rating Presentation functionality complete

--- src/courseComponents/Course/Course.tsx
+++ src/courseComponents/Course/Course.tsx
@@ -5,26 +5,19 @@ interface CourseProps {
 import React from "react";
 import RatingCollector from "../RatingCollector/RatingCollector";
 import Plaque from "../Plaque/Plaque";
-import RatingPresentation, {
-  getRatings,
-} from "../RatingPresentation/RatingPresentation";
+import RatingPresentation from "../RatingPresentation/RatingPresentation";
 
 const Course = ({ name }: CourseProps) => {
   return (
-    <div className="relative w-full bg-black">
+    <div className="relative w-full">
       {/**Course name */}
       <div className="text-2xl font-semibold text-white p-4 w-full">{name}</div>
       {/* Container for right and left components */}
       <div className="flex flex-row">
         <Plaque />
-        {getRatings().then((ratings) => (
-          <RatingPresentation
-            overallRatings={ratings.overallRatings}
-            difficultyRatings={ratings.difficultyRatings}
-            methodsRatings={ratings.methodsRatings}
-            workloadRatings={ratings.workloadRatings}
-          />
-        ))}
+        <div className="flex w-full">
+          <RatingPresentation />
+        </div>
       </div>
     </div>
   );
--- src/courseComponents/RatingPresentation/RatingPresentation.tsx
+++ src/courseComponents/RatingPresentation/RatingPresentation.tsx
@@ -1,30 +1,5 @@
-import React, { useState } from "react";
-import { getAllReviews } from "@/database/db";
-
-interface Ratings {
-  overallRatings: number[];
-  difficultyRatings: number[];
-  methodsRatings: number[];
-  workloadRatings: number[];
-}
-
-export async function getRatings() {
-  const reviews = await getAllReviews();
-
-  const overallRatings = reviews.map((review) => review.overall);
-  const difficultyRatings = reviews.map((reviews) => reviews.difficulty);
-  const methodsRatings = reviews.map((reviews) => reviews.methods);
-  const workloadRatings = reviews.map((reviews) => reviews.workload);
-
-  const ratings: Ratings = {
-    overallRatings,
-    difficultyRatings,
-    methodsRatings,
-    workloadRatings,
-  };
-
-  return ratings;
-}
+import React, { useEffect, useState } from "react";
+import { Ratings } from "@/app/types/types";
 
 function getRatingsMean(ratings: number[]) {
   const length = ratings.length;
@@ -36,51 +11,52 @@ function getRatingsMean(ratings: number[]) {
   return mean;
 }
 
-function RatingPresentation(ratings: Ratings) {
+function RatingPresentation() {
   const maxRating = 5;
+  const [ratings, setRatings] = useState<Ratings | null>(null);
 
-  const reviews = getRatings();
+  useEffect(() => {
+    fetch("/api/ratings", { method: "GET" })
+      .then((res) => res.json())
+      .then((data: Ratings) => setRatings(data))
+      .catch((error) => console.error("Failed to fetch ratings:", error));
+  }, []);
 
-  const [overAllRatingsMean, setOverallMean] = useState(
-    getRatingsMean(ratings.overallRatings)
-  );
-  const [difficultyRatingsMean, setDifficultyMean] = useState(
-    getRatingsMean(ratings.difficultyRatings)
-  );
-  const [methodsRatingsMean, setMethodsMean] = useState(
-    getRatingsMean(ratings.methodsRatings)
-  );
-  const [workloadRatingsMean, setRatingsMean] = useState(
-    getRatingsMean(ratings.workloadRatings)
-  );
+  if (!ratings)
+    return (
+      <div className="flex-1">
+        <p>Loading...</p>
+      </div>
+    );
 
   return (
-    <div className="flex-1 flex-wrap">
+    <div className="flex flex-col w-full items-center p-4 space-y-2">
       {/*Overall ratings*/}
-      <div className="flex-1 items-center justify-center">
+      <div className="flex-1 text-white items-center justify-center">
         <p>
-          Course overall: {overAllRatingsMean}/{maxRating}
+          Course overall: {getRatingsMean(ratings.overallRatings)}/{maxRating}
         </p>
         <p>from {ratings.overallRatings.length} ratings</p>
       </div>
       {/*difficulty ratings*/}
-      <div className="flex-1 items-center justify-center">
+      <div className="flex-1 text-white items-center justify-center">
         <p>
-          Course difficulty: {difficultyRatingsMean}/{maxRating}
+          Course difficulty: {getRatingsMean(ratings.difficultyRatings)}/
+          {maxRating}
         </p>
         <p>from {ratings.difficultyRatings.length} ratings</p>
       </div>
       {/*methods ratings*/}
-      <div className="flex-1 items-center justify-center">
+      <div className="flex-1 text-white items-center justify-center">
         <p>
-          Course methods: {methodsRatingsMean}/{maxRating}
+          Course methods: {getRatingsMean(ratings.methodsRatings)}/{maxRating}
         </p>
         <p>from {ratings.methodsRatings.length} ratings</p>
       </div>
       {/*workload ratings*/}
-      <div className="flex-1 items-center justify-center">
+      <div className="flex-1 text-white items-center justify-center">
         <p>
-          Course workload: {workloadRatingsMean}/{maxRating}
+          Course workload: {getRatingsMean(ratings.workloadRatings)}/{maxRating}
         </p>
         <p>from {ratings.workloadRatings.length} ratings</p>
       </div>
