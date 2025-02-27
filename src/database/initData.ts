93823e299c604f97d47818593f9b6d9157646b8d
Urho <urho.lohikoski@gmail.com>
2025-02-27 22:49:47 +0200

    Users 1-20 added for presentation

--- src/database/initData.ts
+++ src/database/initData.ts
@@ -24,6 +24,126 @@ const testUsers: User[] = [
     username: "charlie_dev",
     password: "mypassword123",
   },
+  {
+    userID: 4,
+    email: "kayttis1@example.com",
+    username: "kayttis1",
+    password: "kayttis1",
+  },
+  {
+    userID: 5,
+    email: "kayttis2@example.com",
+    username: "kayttis2",
+    password: "kayttis2",
+  },
+  {
+    userID: 6,
+    email: "kayttis3@example.com",
+    username: "kayttis3",
+    password: "kayttis3",
+  },
+  {
+    userID: 7,
+    email: "kayttis4@example.com",
+    username: "kayttis4",
+    password: "kayttis4",
+  },
+  {
+    userID: 8,
+    email: "kayttis5@example.com",
+    username: "kayttis5",
+    password: "kayttis5",
+  },
+  {
+    userID: 9,
+    email: "kayttis6@example.com",
+    username: "kayttis6",
+    password: "kayttis6",
+  },
+  {
+    userID: 10,
+    email: "kayttis7@example.com",
+    username: "kayttis7",
+    password: "kayttis7",
+  },
+  {
+    userID: 11,
+    email: "kayttis8@example.com",
+    username: "kayttis8",
+    password: "kayttis8",
+  },
+  {
+    userID: 12,
+    email: "kayttis9@example.com",
+    username: "kayttis9",
+    password: "kayttis9",
+  },
+  {
+    userID: 13,
+    email: "kayttis10@example.com",
+    username: "kayttis10",
+    password: "kayttis10",
+  },
+  {
+    userID: 14,
+    email: "kayttis11@example.com",
+    username: "kayttis11",
+    password: "kayttis11",
+  },
+  {
+    userID: 15,
+    email: "kayttis12@example.com",
+    username: "kayttis12",
+    password: "kayttis12",
+  },
+  {
+    userID: 16,
+    email: "kayttis13@example.com",
+    username: "kayttis13",
+    password: "kayttis13",
+  },
+  {
+    userID: 17,
+    email: "kayttis14@example.com",
+    username: "kayttis14",
+    password: "kayttis14",
+  },
+  {
+    userID: 18,
+    email: "kayttis15@example.com",
+    username: "kayttis15",
+    password: "kayttis15",
+  },
+  {
+    userID: 19,
+    email: "kayttis16@example.com",
+    username: "kayttis16",
+    password: "kayttis16",
+  },
+  {
+    userID: 20,
+    email: "kayttis17@example.com",
+    username: "kayttis17",
+    password: "kayttis17",
+  },
+  {
+    userID: 21,
+    email: "kayttis18@example.com",
+    username: "kayttis18",
+    password: "kayttis18",
+  },
+  {
+    userID: 22,
+    email: "kayttis19@example.com",
+    username: "kayttis19",
+    password: "kayttis19",
+  },
+  {
+    userID: 23,
+    email: "kayttis20@example.com",
+    username: "kayttis20",
+    password: "kayttis20",
+  }
 ];
 
 export const testReviews: Review[] = [
